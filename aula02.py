471b623daa11ed19233e7ec248e9e48e3d84af4e
estephanycarolina <estephanycarolina12@gmail.com>
2024-12-03 09:54:03 -0300

    Add files via upload

---
+++ aula02.py
@@ -0,0 +1,10 @@
+print ('Olá, Bom dia, tudo bem?') 
+nome_usuario = 'estephany carolina' # nurap aula de py para conhecimento em informatica -> str = string
+idade_usuario = 21 # tipo -> int = integer
+altura_usuario = 1.55 # tipo -> float = ponto flutuante
+maior_idade = True # tipo -> bool = boolean
+print('Seja bem-vindo', nome_usuario) 
+print('sua idade é', idade_usuario, 'anos')
+print(' sua altura é', altura_usuario 'metros')
+print('Você é maior de idade?', maior_idade)
+       
